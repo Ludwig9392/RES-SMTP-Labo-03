c417a0ac75bc44e8852ca7b5a4e4ac1cf823198d
Ludwig9392 <loic.frueh@gmail.com>
2018-04-16 13:15:40 +0200

    added partial implementation of the Config Manager class and interface

--- mailPranksGenerator/src/main/java/config/ConfigManager.java
+++ mailPranksGenerator/src/main/java/config/ConfigManager.java
@@ -1,4 +1,9 @@
 package config;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 /**
  * A class that managed all the configurations files infos and use them for this
  * Project. (Parse config info in order to apply them in this Java Project).
@@ -6,5 +11,26 @@ package config;
  * @author Frueh Loïc
  */
 
-public class ConfigManager {
+public class ConfigManager implements IConfigManager {
+
+    private Properties properties;
+
+    public ConfigManager() throws IOException {
+        properties = getProperties("./config/config.properties");
+    }
+
+    public Properties getProperties(String fileName) throws IOException {
+        FileInputStream file = new FileInputStream(fileName);
+        Properties properties = new Properties();
+        properties.load(file);
+        return properties;
+    }
+
+    public String getSmtpServerAdresse() {
+        return properties.getProperty("smtpServer");
+    }
+
+    public int getSmtpServerListenPort() {
+        return Integer.parseInt(properties.getProperty("smtpListenPort"));
+    }
 }
--- mailPranksGenerator/src/main/java/config/IConfigManager.java
+++ mailPranksGenerator/src/main/java/config/IConfigManager.java
@@ -1,4 +1,12 @@
 package config;
 
+import java.io.IOException;
+import java.util.Properties;
+
 public interface IConfigManager {
+
+    public Properties getProperties(String fileName) throws IOException;
+    public String getSmtpServerAdresse();
+    public int getSmtpServerListenPort();
+
 }
