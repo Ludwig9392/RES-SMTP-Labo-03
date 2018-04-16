d2fc0c44f6e26b0eaa97a1cffe9afb9e4b04079d
Ludwig9392 <loic.frueh@gmail.com>
2018-04-16 10:25:58 +0200

    added getters and setters in class Mail

--- mailPranksGenerator/src/main/java/model/mail/Mail.java
+++ mailPranksGenerator/src/main/java/model/mail/Mail.java
@@ -12,4 +12,45 @@ public class Mail {
     private String[] cc;
     private String subject;
     private String message;
+
+    public String getFrom() {
+        return from;
+    }
+
+    public String[] getTo() {
+        return to;
+    }
+
+    public String[] getCc() {
+        return cc;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public void setTo(String[] to) {
+        this.to = to;
+    }
+
+    public void setCc(String[] cc) {
+        this.cc = cc;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
 }
