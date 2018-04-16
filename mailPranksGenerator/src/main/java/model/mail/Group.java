989d884f6d190e619810317d8e73d3390b8fa18c
Ludwig9392 <loic.frueh@gmail.com>
2018-04-16 08:21:08 +0200

    added attributes getters/setters and basics to class Group

--- mailPranksGenerator/src/main/java/model/mail/Group.java
+++ mailPranksGenerator/src/main/java/model/mail/Group.java
@@ -1,5 +1,8 @@
 package model.mail;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * A class that represents a Group of Person in terms of e-mail properties.
  * A Group is just a list of Person. Each Person have a complete name
@@ -9,4 +12,39 @@ package model.mail;
  */
 
 public class Group {
+
+    private final List<Person> group = new ArrayList<Person>();
+
+    public int getNumberOfPerson() {
+        return group.size();
+    }
+
+    public void clear() {
+        group.clear();
+    }
+
+    public void addPerson(Person person){
+        group.add(person);
+    }
+
+    public void addMultiplePersons(Person... persons) {
+        for (Person p : persons) {
+            addPerson(p);
+        }
+    }
+
+    public void addMultiplePersons(List<Person> persons){
+        group.addAll(persons);
+    }
+
+    public List<Person> getGroup() {
+        ArrayList<Person> group = new ArrayList<Person>();
+        group.addAll(this.group);
+        return group;
+    }
+
+    public void setGroup(List<Person> group) {
+        clear();
+        addMultiplePersons(group);
+    }
 }
