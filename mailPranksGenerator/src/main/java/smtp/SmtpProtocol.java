6fd3b34a2a7327101ca1e17b6aa3c1e90f249ece
Ludwig9392 <loic.frueh@gmail.com>
2018-04-17 13:05:22 +0200

    added a Implementation of the SmtpProtocol class that contains useful constants

--- mailPranksGenerator/src/main/java/smtp/SmtpProtocol.java
+++ mailPranksGenerator/src/main/java/smtp/SmtpProtocol.java
@@ -7,4 +7,19 @@ package smtp;
  */
 
 public class SmtpProtocol {
+    public final static String CMD_HELLO = "EHLO";
+    public final static String CMD_MAIL = "MAIL FROM:";
+    public final static String CMD_RCPT = "RCPT TO:";
+    public final static String CMD_DATA = "DATA";
+    public final static String CMD_QUIT = "quit";
+
+    public final static String FROM = "From:";
+    public final static String TO = "To:";
+    public final static String CC = "Cc:";
+    public final static String SUBJECT = "Subject:";
+
+    public final static String RETURN = "\r\n";
+    public final static String END_MESSAGE = "\r\n.\r\n";
+
+
 }
