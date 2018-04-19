c15a2b38ed3f3366d9ad85bfbacde44eec0aee35
DDejviDD <dejvid.muaremi@heig-vd.ch>
2018-04-19 19:15:27 +0200

    Commenting the ISmtpClient.java

--- mailPranksGenerator/src/main/java/smtp/ISmtpClient.java
+++ mailPranksGenerator/src/main/java/smtp/ISmtpClient.java
@@ -1,10 +1,20 @@
 package smtp;
 
 import model.mail.Mail;
-
 import java.io.IOException;
 
+/***
+ * This interface is used to send an email with a smtp client.
+ * The implementation of the interface decides how the email should be sent.
+ *
+ * @author Loïc Frueh, Dejvid Muaremi
+ */
 public interface ISmtpClient {
 
+    /***
+     * When this method is invoked it will send an email.
+     * @param email the email to send to a smtp server.
+     * @throws IOException when a reader or a writer has a IO exception.
+     */
     public void sendEmail(Mail email) throws IOException;
 }
