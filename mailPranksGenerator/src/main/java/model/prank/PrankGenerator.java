5e6da5373e5a8c8d7a83ff615ff3442dd28e6767
DDejviDD <dejvid.muaremi@heig-vd.ch>
2018-04-19 13:23:52 +0200

    Logging of the prank generator.
    The file is now logged.
    The infos message are in a INFO logger.
    The errors and exceptions are logged as a SEVERE message.

--- mailPranksGenerator/src/main/java/model/prank/PrankGenerator.java
+++ mailPranksGenerator/src/main/java/model/prank/PrankGenerator.java
@@ -5,6 +5,7 @@ import model.mail.Group;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.logging.*;
 
 /**
  * A class that modelize a Generator of forged-email.
@@ -14,6 +15,7 @@ import java.util.List;
 public class PrankGenerator {
     private IConfigManager config;
     private final static int MIN_NUMBER_PERSON_PER_GROUP = 3;
+    private static final Logger LOG = Logger.getLogger(PrankGenerator.class.getName());
 
     public PrankGenerator(IConfigManager config) {
         this.config = config;
@@ -21,17 +23,20 @@ public class PrankGenerator {
 
     public List<Group> createPrankGroups(Group victims, int numberOfGroups) {
         List<Group> groupList = new ArrayList<Group>();
+        LOG.info("BEGIN : Creation of the prank groups.");
         for (int i = 0; i < numberOfGroups; ++i) {
             groupList.add(new Group());
         }
-
+        LOG.info("END : Creation of the prank groups.");
         victims.shuffle();
 
         int groupNumber = 0;
+        LOG.info("BEGIN : Add victimes to the prank groups.");
         while (victims.getNumberOfPerson() > 0) {
             groupList.get(groupNumber).addPerson(victims.removePerson(0));
             groupNumber = (groupNumber + 1) % numberOfGroups;
         }
+        LOG.info("END : Add victimes to the prank groups.");
 
         return groupList;
     }
@@ -40,19 +45,23 @@ public class PrankGenerator {
         List<Prank> pranks = new ArrayList<Prank>();
 
         int numberOfGroups = config.getNumberOfGroups();
-        int numberOfvictims = config.getVictims().getNumberOfPerson();
+        int numberOfVictims = config.getVictims().getNumberOfPerson();
 
-        if (numberOfvictims / numberOfGroups < MIN_NUMBER_PERSON_PER_GROUP){
-            numberOfGroups = numberOfvictims / 3;
-            System.out.println("Sorry but with your configuration we can only generate " + numberOfGroups + "groups maximum, edit your configuration files in consequence.");
+        if (numberOfVictims / numberOfGroups < MIN_NUMBER_PERSON_PER_GROUP){
+            numberOfGroups = numberOfVictims / 3;
+            LOG.log(Level.SEVERE, "Sorry but with your configuration we can only generate {0} groups maximum, edit your configuration files in consequence.", numberOfGroups);
             return pranks; // Empty list
         }
-
+        
         List<Group> groupsToPrank = createPrankGroups(config.getVictims(), numberOfGroups);
-
+        LOG.info(String.valueOf(numberOfGroups) + " groups have been created with at least " + String.valueOf(MIN_NUMBER_PERSON_PER_GROUP) + "persons in it.");
+        
         List<String> messages = config.getMessages();
         Collections.shuffle(messages);
+        LOG.info("The message have been taken from the config file and shuffled.");
+        
         int messageIndex = 0;
+        LOG.info("BEGIN : Generation of the pranks.");
         for (Group group : groupsToPrank) {
             group.shuffle();
 
@@ -68,7 +77,7 @@ public class PrankGenerator {
 
             pranks.add(prank);
         }
-
+        LOG.info("END : Generation of the pranks.");
         return pranks;
     }
 }
